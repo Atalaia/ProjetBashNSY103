078a719857ffea06d19ba77f59f7ab7e9ebcefdf
Atalaia <maria.soage01@gmail.com>
2019-02-02 20:14:03 +0100

    Add files via upload

---
+++ romantodec.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Méthode qui fait la conversion de romans à décimaux 
+ 
+roman_to_dec() {
+  local rnum=$1
+  local n=0
+  local prev=0
+ 
+  for ((i=${#rnum}-1;i>=0;i--))
+  do
+    case "${rnum:$i:1}" in
+    M)  a=1000 ;;
+    D)  a=500 ;;
+    C)  a=100 ;;
+    L)  a=50 ;;
+    X)  a=10 ;;
+    V)  a=5 ;;
+    I)  a=1 ;;
+    esac
+ 
+    if [[ $a -lt $prev ]]
+    then
+       let n-=a
+    else
+       let n+=a
+    fi
+ 
+    prev=$a
+  done
+ 
+  echo "$rnum = $n"
+}
+
+ 
+# Tests de vérification de la méthode
+
+roman_to_dec MCMXC
+roman_to_dec MMVIII
+roman_to_dec MDCLXVI
+roman_to_dec CCCD
+roman_to_dec LIX
+roman_to_dec MCCIV
